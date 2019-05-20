c0ae06d923c2d5b2dbb4422d1a340992c302ab0f
tempoo5 <49492894+tempoo5@users.noreply.github.com>
2019-05-20 21:31:05 +0200

    Add files via upload

--- Scoreboard/src/de/scoreboard/listener/ScoreboardListener.java
+++ Scoreboard/src/de/scoreboard/listener/ScoreboardListener.java
@@ -9,66 +9,94 @@ import org.bukkit.scoreboard.DisplaySlot;
 import org.bukkit.scoreboard.Objective;
 import org.bukkit.scoreboard.Scoreboard;
 
+import de.scoreboard.main.Scoreboards;
+
 public class ScoreboardListener implements Listener{
+	
+	int sched;
+	
+	private final long SCOREBOARD_DELAY = 20*20;
 	
 	@EventHandler
 	public void handlePlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
-		event.setJoinMessage("");
+		this.updateScoreboard(player);
+	
+		if(!Bukkit.getScheduler().isCurrentlyRunning(sched)) {
+			sched = Bukkit.getScheduler().scheduleSyncRepeatingTask(Scoreboards.getInstance(), new Runnable() {
+			
+			@Override
+			public void run() {
+				for (Player player : Bukkit.getOnlinePlayers()) {
+					updateScoreboard(player);
+				}
+				
+			}
+
+		}, 0, SCOREBOARD_DELAY);
+	}
 		
+	}
+
+	private void updateScoreboard(Player player) {
+			
 		Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
 		Objective objectiv = board.registerNewObjective("abcd", "abcd");
+		
 		objectiv.setDisplaySlot(DisplaySlot.SIDEBAR);
 		objectiv.setDisplayName("ß6ßlEnderGamesMC.de");
-		objectiv.getScore("ßcßlRang:").setScore(9);
+		
+
+		objectiv.getScore("ßcßlName:").setScore(8);
+		objectiv.getScore("ßbßl" + player.getName()).setScore(7);
+		objectiv.getScore("ßcßl").setScore(6);
+		objectiv.getScore("ßcßlSpieler:").setScore(5);
+		objectiv.getScore("ßcßl" + Bukkit.getOnlinePlayers().size() + "ßbßl/ßcßl" + Bukkit.getMaxPlayers()).setScore(4);
+		objectiv.getScore("").setScore(3);
+		objectiv.getScore("ßcßlRang:").setScore(2);
 		if(player.hasPermission("Prefix.Owner")) {
-			objectiv.getScore("ß4ßlOwner").setScore(8);
-		} else if(player.hasPermission("Prefix.Spieler")) {
-			objectiv.getScore("ßcßlFreund").setScore(8);
-		} else if(player.hasPermission("Prefix.Freund")) {
-			objectiv.getScore("ßcßlSpieler").setScore(8);
-		} else if(player.hasPermission("Prefix.Youtuber")) {
-			objectiv.getScore("ß5ßlYoutuber").setScore(8);
-		} else if(player.hasPermission("Prefix.MasterBuilder")) {
-			objectiv.getScore("ß1ßlMBuilder").setScore(8);
-		} else if(player.hasPermission("Prefix.DI/TSAdmin")) {
-			objectiv.getScore("ß6ßlDI/TSAdmin").setScore(8);
-		} else if(player.hasPermission("Prefix.DevAzubi")) {
-			objectiv.getScore("ßdßlDevAzubi").setScore(8);
-		} else if(player.hasPermission("Prefix.BuildAzubi")) {
-			objectiv.getScore("ßdßlBuildAzubi").setScore(8);
-		} else if(player.hasPermission("Prefix.Developer")) {
-			objectiv.getScore("ßbßlDeveveloper").setScore(8);
+			objectiv.getScore("ß4ßlOwner").setScore(1);
 		} else if(player.hasPermission("Prefix.Admin")) {
-			objectiv.getScore("ßcßlAdmin").setScore(8);
+			objectiv.getScore("ßcßlAdmin").setScore(1);
+		} else if(player.hasPermission("Prefix.Developer")) {
+			objectiv.getScore("ßbßlDeveloper").setScore(1);
+		} else if(player.hasPermission("Prefix.DI/TSAdmin")) {
+			objectiv.getScore("ß6ßlDI/TSAdmin").setScore(1);
+		} else if(player.hasPermission("Prefix.MasterBuilder")) {
+			objectiv.getScore("ß1ßlMBuilder").setScore(1);
 		} else if(player.hasPermission("Prefix.Builder")) {
-			objectiv.getScore("ßaßlBuilder").setScore(8);
+			objectiv.getScore("ßaßlBuilder").setScore(1);
+		} else if(player.hasPermission("Prefix.DevAzubi")) {
+			objectiv.getScore("ßdßlDevAzubi").setScore(1);
+		} else if(player.hasPermission("Prefix.BuildAzubi")) {
+			objectiv.getScore("ßdßlBuildAzubi").setScore(1);
 		} else if(player.hasPermission("Prefix.Moderator")) {
-			objectiv.getScore("ß3ßlModerator").setScore(8);
+			objectiv.getScore("ß3ßlModerator").setScore(1);
 		} else if(player.hasPermission("Prefix.Supporter")) {
-			objectiv.getScore("ß2ßlSupporter").setScore(8);
+			objectiv.getScore("ß2ßlSupporter").setScore(1);
+		} else if(player.hasPermission("Prefix.Freund")) {
+			objectiv.getScore("ßcßlFreund").setScore(1);
 		} else if(player.hasPermission("Prefix.Youtuber+")) {
-			objectiv.getScore("ß5ßlYoutuberß4ßl+").setScore(8);
-		} else if(player.hasPermission("Prefix.Legende")) {
-			objectiv.getScore("ßcßlLegende").setScore(8);
-		} else if(player.hasPermission("Prefix.Titan")) {
-			objectiv.getScore("ß9ßlTitan").setScore(8);
-		} else if(player.hasPermission("Prefix.Drache")) {
-			objectiv.getScore("ß4ßlDrache").setScore(8);
+			objectiv.getScore("ß5ßlYoutuberß4ßl+").setScore(1);
 		} else if(player.hasPermission("Prefix.Champ")) {
-			objectiv.getScore("ßeßlChampion").setScore(8);
+			objectiv.getScore("ßeßlChampion").setScore(1);
+		} else if(player.hasPermission("Prefix.Drache")) {
+			objectiv.getScore("ß4ßlDrache").setScore(1);
+		} else if(player.hasPermission("Prefix.Titan")) {
+			objectiv.getScore("ß9ßlTitan").setScore(1);
+		} else if(player.hasPermission("Prefix.Youtuber")) {
+			objectiv.getScore("ß5ßlYoutuber").setScore(1);
+		} else if(player.hasPermission("Prefix.Legende")) {
+			objectiv.getScore("ßcßlLegende").setScore(1);
 		} else if(player.hasPermission("Prefix.Ultra")) {
-			objectiv.getScore("ßbßlUltra").setScore(8);
+			objectiv.getScore("ßbßlUltra").setScore(1);
 		} else if(player.hasPermission("Prefix.Premium")) {
-			objectiv.getScore("ß6ßlPremium").setScore(8);
+			objectiv.getScore("ß6ßlPremium").setScore(1);
+		} else if(player.hasPermission("Prefix.Spieler")) {
+			objectiv.getScore("ß7ßlSpieler").setScore(1);
 		}
-		objectiv.getScore("").setScore(7);
-		objectiv.getScore("ßcßlName:").setScore(6);
-		objectiv.getScore("ßbßl" + player.getName()).setScore(5);
-		objectiv.getScore("").setScore(4);
-		objectiv.getScore("ßcßlSpieler:").setScore(3);
-		objectiv.getScore("ßcßl" + player.getServer().getMaxPlayers()).setScore(2);
-		objectiv.getScore("ßaViel Spaﬂ auf dem Server!").setScore(1);
+		
 		player.setScoreboard(board);
 	}
+
 }
--- Scoreboard/src/de/scoreboard/main/Scoreboards.java
+++ Scoreboard/src/de/scoreboard/main/Scoreboards.java
@@ -10,8 +10,11 @@ public class Scoreboards extends JavaPlugin {
 	
 	private static Scoreboards plugin;
 	
+	public static Scoreboards instance;
+	
 	public void onEnable() {
 		plugin = this;
+		instance = this;
 		
 		PluginManager pluginManager = Bukkit.getPluginManager();
 		pluginManager.registerEvents(new ScoreboardListener(), this);
@@ -21,7 +24,9 @@ public class Scoreboards extends JavaPlugin {
 	public static Scoreboards getPlugin() {
 		return plugin;
 	}
-	
-	
+
+	public static Scoreboards getInstance() {
+		return instance;
+	}
 	
 }
