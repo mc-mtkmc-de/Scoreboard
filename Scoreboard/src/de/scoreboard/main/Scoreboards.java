329dfc0f24b52c0bf5d8ddbf9a2245741ae5360d
tempoo5 <49492894+tempoo5@users.noreply.github.com>
2019-06-16 10:01:46 +0200

    Add files via upload

--- Scoreboard/src/de/scoreboard/main/Scoreboards.java
+++ Scoreboard/src/de/scoreboard/main/Scoreboards.java
@@ -23,6 +23,8 @@ public class Scoreboards extends JavaPlugin implements Listener {
 	
 	private final long SCOREBOARD_DELAY = 20*20;
 	
+	Scoreboard board;
+	
 	public void onEnable() {
 		instance = this;
 		
@@ -59,29 +61,29 @@ public class Scoreboards extends JavaPlugin implements Listener {
 	public void handlePlayerJoin(PlayerJoinEvent event) {
 		Player player = event.getPlayer();
 		this.updateScoreboard(player);
+		setPrefix(event.getPlayer());
 
 	
-		if(!Bukkit.getScheduler().isCurrentlyRunning(sched)) {
-			sched = Bukkit.getScheduler().scheduleSyncRepeatingTask(Scoreboards.getInstance(), new Runnable() {
+		Bukkit.getScheduler().scheduleSyncRepeatingTask(Scoreboards.getPlugin(), new Runnable() {
 			
 			@Override
 			public void run() {
 				for (Player player : Bukkit.getOnlinePlayers()) {
-					updateScoreboard(player);
+					setPrefix(player);
 					playerBalance(player, null);
+					Bukkit.getOnlinePlayers().size();
 					
 				}
 				
 			}
 
 		}, 0, SCOREBOARD_DELAY);
-	}
 		
 	}
 
 	private void updateScoreboard(Player player) {
 			
-		Scoreboard board = Bukkit.getScoreboardManager().getNewScoreboard();
+		board = Bukkit.getScoreboardManager().getNewScoreboard();
 		Objective objectiv = board.registerNewObjective("abcd", "abcd");
 		
 		objectiv.setDisplaySlot(DisplaySlot.SIDEBAR);
@@ -140,10 +142,98 @@ public class Scoreboards extends JavaPlugin implements Listener {
 		objectiv.getScore("§6§lSpieler:").setScore(2);
 		objectiv.getScore("§d§l" + Bukkit.getOnlinePlayers().size() + "§b§l/§c§l" + Bukkit.getMaxPlayers()).setScore(1);
 		
+		board.registerNewTeam("00000Owner");
+		board.registerNewTeam("00001Admin");
+		board.registerNewTeam("00002Dev");
+		board.registerNewTeam("00003MBuilder");
+		board.registerNewTeam("00004Builder");
+		board.registerNewTeam("00005Azubi");
+		board.registerNewTeam("00006Mod");
+		board.registerNewTeam("00007Sup");
+		board.registerNewTeam("00008Freund");
+		board.registerNewTeam("00009YT+");
+		board.registerNewTeam("00010Champ");
+		board.registerNewTeam("00011Drache");
+		board.registerNewTeam("00012Titan");
+		board.registerNewTeam("00013YT");
+		board.registerNewTeam("00014Legende");
+		board.registerNewTeam("00015Ultra");
+		board.registerNewTeam("00016Premium");
+		board.registerNewTeam("00017Spieler");
 		
-		
-		player.setScoreboard(board);
+		board.getTeam("00000Owner").setPrefix("§a[Owner]");
+		board.getTeam("00001Admin").setPrefix("§c[Admin]");
+		board.getTeam("00002Dev").setPrefix("§b[Dev]");
+		board.getTeam("00003MBuilder").setPrefix("§2[MBuilder]");
+		board.getTeam("00004Builder").setPrefix("§a[Builder]");
+		board.getTeam("00005Azubi").setPrefix("§d[Azubi]");
+		board.getTeam("00006Mod").setPrefix("§3[Mod]");
+		board.getTeam("00007Sup").setPrefix("§2[Sup]");
+		board.getTeam("00008Freund").setPrefix("§c[Freund]");
+		board.getTeam("00009YT+").setPrefix("§5[YT§4+§5]");
+		board.getTeam("00010Champ").setPrefix("§e[champ]");
+		board.getTeam("00011Drache").setPrefix("§4[Drache]");
+		board.getTeam("00012Titan").setPrefix("§9[Titan]");
+		board.getTeam("00013YT").setPrefix("§5[YT]");
+		board.getTeam("00014Legende").setPrefix("§c[Legende]");
+		board.getTeam("00015Ultra").setPrefix("§b[Ultra]");
+		board.getTeam("00016Premium").setPrefix("§6[Premium]");
+		board.getTeam("00017Spieler").setPrefix("§7[Spieler]");
+				
 	}
+
+	private void setPrefix(Player p) {
+	
+		String team = "";
+	
+		if(p.hasPermission("tab.owner")) {
+		team = "00000Owner";
+		} else if(p.hasPermission("tab.admin")) {
+		team = "00001Admin";
+		} else if(p.hasPermission("tab.dev")) {
+		team = "00002Dev";
+		} else if(p.hasPermission("tab.mbuilder")) {
+		team = "00003MBuilder";
+		} else if(p.hasPermission("tab.builder")) {
+		team = "00004Builder";
+		} else if(p.hasPermission("tab.azubi")) {
+		team = "00005Azubi";
+		} else if(p.hasPermission("tab.mod")) {
+		team = "00006Mod";
+		} else if(p.hasPermission("tab.sup")) {
+		team = "00007Sup";
+		} else if(p.hasPermission("tab.freund")) {
+		team = "00008Freund";
+		} else if(p.hasPermission("tab.yt+")) {
+		team = "00009YT+";
+		} else if(p.hasPermission("tab.champ")) {
+		team = "00010Champ";
+		}  else if(p.hasPermission("tab.drache")) {
+		team = "00011Drache";
+		} else if(p.hasPermission("tab.titan")) {
+		team = "00012Titan";
+		} else if(p.hasPermission("tab.yt")) {
+		team = "00013YT";
+		} else if(p.hasPermission("tab.legende")) {
+		team = "00014Legende";
+		} else if(p.hasPermission("tab.ultra")) {
+		team = "00015Ultra";
+		} else if(p.hasPermission("tab.premium")) {
+		team = "00016Premium";
+		} else {
+		team = "00017Spieler";
+	}
+	
+	board.getTeam(team).addPlayer(p);
+	p.setDisplayName(board.getTeam(team).getPrefix() + p.getName());
+	
+	for(Player all : Bukkit.getOnlinePlayers()) {
+		all.setScoreboard(board);
+		}
+	
+	}
+
+	
 
 	
 	public static Scoreboards getPlugin() {
